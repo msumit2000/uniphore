ba95178efdf5c8fdca41bbd2fa2ea5fe4ecb9adb
siddhantkadam1 <siddhant.kadam@uniphore.com>
2023-07-20 16:40:21 +0530

    UAC

--- package/udops/uniphore/uniphore/settings.py
+++ package/udops/uniphore/uniphore/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'uniphore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
