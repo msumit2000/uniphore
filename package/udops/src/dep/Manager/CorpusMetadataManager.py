462f838d5ba157f23c04d3673891658155c56dbc
siddhantkadam1 <siddhant.kadam@uniphore.com>
2023-07-20 13:31:55 +0530

    UAC

--- package/udops/src/dep/Manager/CorpusMetadataManager.py
+++ package/udops/src/dep/Manager/CorpusMetadataManager.py
@@ -256,14 +256,14 @@ class CorpusMetadataManager:
         try:
             if corpus_name=="":
                cursor = conn.cursor(cursor_factory=RealDictCursor)
-               cursor.execute("SELECT corpus_id, corpus_name, corpus_type, language, source_type,migration_date, lastupdated_ts, description, acquisition_date, (SELECT teamname FROM cfg_udops_teams_metadata tm WHERE tm.team_id IN (SELECT team_id FROM cfg_udops_teams_acl cta WHERE cta.corpus_id = corpus_metadata.corpus_id ) ) AS team_name FROM corpus_metadata")
+               cursor.execute("SELECT corpus_id, corpus_name, corpus_type, language, source_type, (SELECT teamname FROM cfg_udops_teams_metadata tm WHERE tm.team_id IN (SELECT team_id FROM cfg_udops_teams_acl cta WHERE cta.corpus_id = corpus_metadata.corpus_id ) ) AS team_name FROM corpus_metadata")
                rows = cursor.fetchall()
                conn.commit()
                cursor.close()
                return rows
             else:
                 cursor = conn.cursor(cursor_factory=RealDictCursor)
-                query=f"SELECT corpus_id, corpus_name, corpus_type, language, source_type,migration_date, lastupdated_ts, description, acquisition_date, (SELECT teamname FROM cfg_udops_teams_metadata tm WHERE tm.team_id IN (SELECT team_id FROM cfg_udops_teams_acl cta WHERE cta.corpus_id = corpus_metadata.corpus_id ) ) AS team_name FROM corpus_metadata where corpus_name ='{corpus_name}'"
+                query=f"SELECT corpus_id, corpus_name, corpus_type, language, source_type,(SELECT teamname FROM cfg_udops_teams_metadata tm WHERE tm.team_id IN (SELECT team_id FROM cfg_udops_teams_acl cta WHERE cta.corpus_id = corpus_metadata.corpus_id ) ) AS team_name FROM corpus_metadata where corpus_name ='{corpus_name}'"
 
 #                cursor.execute(f"SELECT corpus_id, corpus_name, corpus_type, language, source_type,migration_date, lastupdated_ts, description, acquisition_date, (SELECT teamname FROM cfg_udops_teams_metadata tm WHERE tm.team_id IN (SELECT team_id FROM cfg_udops_teams_acl cta WHERE cta.corpus_id = corpus_metadata.corpus_id ) ) AS team_name FROM corpus_metadata where corpus_nam ='{corpus_name}'")
                 cursor.execute(query)
