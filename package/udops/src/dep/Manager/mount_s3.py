01d77464a47dd9a53dca1098c82aaab9e47b24c9
Siddhant Kadam <siddhant.kadam@uniphore.com>
2023-08-31 23:46:47 +0530

    UI Updates

--- package/udops/src/dep/Manager/mount_s3.py
+++ package/udops/src/dep/Manager/mount_s3.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 
-def mount_s3_bucket(bucket_name, mount_point):
-    mount_point = "/home/siddhant/mount"+{mount_point}
-    os.makedirs(mount_point, exist_ok=True)
+class mount_s3:
+    def mount_s3_bucket(bucket_name, mount_point):
+        mount_point = "/home/siddhant/mount"+{mount_point}
+        os.makedirs(mount_point, exist_ok=True)
 
-    # Mount the S3 bucket using s3fs
-    cmd = f"mount-s3 {bucket_name}  /home/siddhant/mount/{mount_point}"
-    subprocess.run(cmd, shell=True, check=True)
+        # Mount the S3 bucket using s3fs
+        cmd = f"mount-s3 {bucket_name}  /home/siddhant/mount/{mount_point}"
+        subprocess.run(cmd, shell=True, check=True)
 
-    return mount_point
+        return mount_point
