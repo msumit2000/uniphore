e811ec9876ffedc13e6728244a97d88cd969131e
Siddhant Kadam <siddhant.kadam@uniphore.com>
2023-09-01 01:20:56 +0530

    UI Updates

--- package/udops/src/dep/Manager/mount_s3.py
+++ package/udops/src/dep/Manager/mount_s3.py
@@ -10,7 +10,7 @@ class mount_s3:
         os.makedirs(mount_point, exist_ok=True)
 
         # Mount the S3 bucket using s3fs
-        cmd = f"mount-s3 {bucket_name}  {mount_point}"
+        cmd = f"sudo mount-s3 {bucket_name}  {mount_point}"
         os.system(cmd)
 
         return mount_point
