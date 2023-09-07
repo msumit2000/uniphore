b5ede9255974fae9a87fa9323359d5b9f57985dc
msumit2000 <sumit.mahanwar@dataeaze.io>
2023-09-07 11:16:16 +0530

    change in mount3

--- package/udops/src/dep/Manager/mount_s3.py
+++ package/udops/src/dep/Manager/mount_s3.py
@@ -6,16 +6,22 @@ class mount_s3:
 
     def mount_s3_bucket(self,destination_base_path, mount_point):
         # slicing of bucket name
-        path = destination_base_path.replace("s3://", "")
-        first_index = path.find("/")
-        if first_index != -1:
-            bucket_name = path[:first_index] + ":/" + path[first_index + 1:]
-        else:
-            bucket_name = destination_base_path
+        try:
+            path = destination_base_path.replace("s3://", "")
+            first_index = path.find("/")
+            if first_index != -1:
+                bucket_name = path[:first_index] + ":/" + path[first_index + 1:]
+            else:
+                bucket_name = destination_base_path
 
-        mount_point = "/home/ubuntu/mount/"+str(mount_point)
-        os.makedirs(mount_point, exist_ok=True)
+            mount_point = "/home/ubuntu/mount/"+str(mount_point)
+            os.makedirs(mount_point, exist_ok=True)
 
-        command = f"s3fs {bucket_name} {mount_point}"
-        subprocess.run(command, shell=True, check=True)
-        return mount_point
+            command = f"s3fs {bucket_name} {mount_point}"
+            subprocess.run(command, shell=True, check=True)
+            return mount_point
+
+        except Exception as e:
+            err = str(e)
+            print(err)
+            return mount_point
