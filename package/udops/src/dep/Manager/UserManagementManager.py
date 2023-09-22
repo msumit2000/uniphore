adddbf8379f5e30829813e3a013aa3a5faf3dea5
msumit2000 <sumit.mahanwar@dataeaze.io>
2023-09-22 05:05:23 -0700

    list_admin

--- package/udops/src/dep/Manager/UserManagementManager.py
+++ package/udops/src/dep/Manager/UserManagementManager.py
@@ -132,7 +132,7 @@ class UserManagementManager:
                 row = cursor.fetchone()
                 team_id = row['user_name']
                 admin_name.append(team_id)
-                
+
             return admin_name
 
         except Exception as e:
@@ -142,6 +142,7 @@ class UserManagementManager:
         try:
             conn = connection.get_connection()
             cursor = conn.cursor(cursor_factory=RealDictCursor)
+            print(f"username-->{username}")
 
             userid = []
             for user in username:
@@ -150,6 +151,7 @@ class UserManagementManager:
                 row = cursor.fetchone()
                 user_id = row['user_id']
                 userid.append(user_id)
+            print(f"user_id--->{userid}")
 
 
             # team_id respective to team
@@ -157,12 +159,15 @@ class UserManagementManager:
             cursor.execute(query)
             row = cursor.fetchone()
             team_id = row['team_id']
+            print(f"team_id--->{team_id}")
 
             # fetching admin_id with respect to given team
             que1 = f"select admin_id from cfg_udops_teams_admin where team_id = {team_id}"
             cursor.execute(que1)
             ro1 = cursor.fetchall()
             admin_ids = [row.get('admin_id') for row in ro1]
+
+            print(f"admin_id--->{admin_ids}")
 
             # check weather admin_id is present in user_id array.
             array1 = []
@@ -189,6 +194,7 @@ class UserManagementManager:
 
             conn.commit()
             cursor.close()
+            print(f"array2---->{array2}")
             return array2
         except Exception as e:
             error = str(e)
